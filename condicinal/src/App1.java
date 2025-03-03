ac1a108b79166604ed7f114f18e01255171e1025
San096 <sanderleycc2022@alu.ufc.br>
2025-03-03 16:21:15 -0300

    a

---
+++ condicinal/src/App1.java
@@ -0,0 +1,24 @@
+// Fazer um programa para ler um número inteiro e dizer se este número é par ou ímpar. 
+
+import java.util.*;
+
+Class App1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int x =  sc.nextInt();
+
+            if( x % 2 == 0){
+                System.out.println("Par");
+            }
+            else{
+                System.out.println("Ímpar");
+            }
+
+
+
+    }
+}
+
+
